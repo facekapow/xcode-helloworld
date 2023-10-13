73b5d9b1be3ad5acdb0059c77661231b87ec320f
Ariel Abreu <facekapow@outlook.com>
2023-10-13 00:38:40 -0400

    Regenerate project with Xcode 11.7
    Note that I had to add `/usr/lib/swift` to the RPATH for the Swift libs
    to load correctly under Darling.

--- HelloWorld/AppDelegate.swift
+++ HelloWorld/AppDelegate.swift
@@ -2,14 +2,14 @@
 //  AppDelegate.swift
 //  HelloWorld
 //
-//  Created by Ariel Abreu on 7/31/23.
+//  Created by Ariel Abreu on 10/13/23.
 //
 
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    @IBOutlet var window: NSWindow!
+    @IBOutlet weak var window: NSWindow!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -17,9 +17,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
-    }
-
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
     }
 }
